9f92cc19ba93c41842e85c30a8b405d0e6d9b0cc
Jeet <jeetbaldha12@gmail.com>
2024-01-09 02:04:49 +0530

    solve error

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,10 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn.jSX";
+import LogoutBtn from './Header/LogoutBtn'
 import Button from "./Button";
 import Input from "./Input";
-import RTE from "./RTE";
+import RTE from "./RTE";    
 import Signup from "./SignUp"
 import  Login  from "./Login";
 import PostForm from "./post-form/PostForm";
